9440282f20adaf4447858a426f90492420f40d50
morellana <morellana@SAL-MORELLANA>
2020-07-16 16:29:55 -0300

    first commit

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const URI = 'mongodb://localhost/mern-crud-test';
 
 mongoose.connect(URI)
-  .then(db => console.log('Db is connected'))
+  .then(db => console.log('Base de datos conectada'))
   .catch(error => console.error(error));
 
 module.exports = mongoose;
